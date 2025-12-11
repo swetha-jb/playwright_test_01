010c8b45a4a14abefc2d82e8e9d2777c5c24b80c
root <root@vvdn-sfit>
2025-12-11 14:15:43 +0530

    Pushed Successfully

--- src/locators/flipkart.ts
+++ src/locators/flipkart.ts
@@ -1,5 +1,5 @@
 class Login_Locator{
-    click_login='xpath=//span[text()="Login"]'
+    click_login='xpath=//span[text()="Login1"]'
     click_to_enter_number='xpath=//span[text()="Enter Email/Mobile number"]/../..//input'
     click_on_rqst_otp='xpath=//button[text()="Request OTP"]'
 }
